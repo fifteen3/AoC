94f167dc0ac05d25702dd3fefd3296be9062f004
Carlo Costantini <carlo@costantini.xyz>
2021-01-02 10:06:28 -0500

    change how data set is iterated over and accept rise and run as part of parameters to attempt

--- 2020/day3/src/main.rs
+++ 2020/day3/src/main.rs
@@ -39,18 +39,25 @@ fn main() {
             println!("part1 matches: {:?}", matches);
             println!("filename: {}", filename);
             let dataset = dataset::read_in_lines(filename);
-            let total_valid_passwords = part1::attempt(dataset);
+            let total_valid_passwords = part1::attempt(dataset, 1, 3);
             println!("{}", total_valid_passwords);
         }
     }
     if let Some(matches) = matches.subcommand_matches("part2") {
         if matches.is_present("file") {
             let filename = matches.value_of("file").unwrap().to_string();
-            println!("part2 matches: {:?}", matches);
-            println!("filename: {}", matches.value_of("file").unwrap().to_string());
+            //println!("part2 matches: {:?}", matches);
+           // println!("filename: {}", matches.value_of("file").unwrap().to_string());
             let dataset = dataset::read_in_lines(filename);
-            //let total_valid_passwords = part2::attempt(dataset);
-           // println!("{}", total_valid_passwords);
+            let mut all_hits = vec![];
+            let params = vec![(1,1),(1,3),(1,5),(1,7),(2,1)];
+            for param in params {
+                let (rise, run) = param;
+                let hits = part1::attempt(dataset.clone(), rise, run);
+                all_hits.push(hits);
+            }
+            let ints: f64 = all_hits.into_iter().product();
+            println!("{}",  ints);
         }
     }
 }
--- 2020/day3/src/part1.rs
+++ 2020/day3/src/part1.rs
@@ -9,7 +9,12 @@ impl PartialEq for TreeMark {
         self.x == other.x && self.y == other.y
     }
 }
-pub fn attempt(dataset: Vec<String>) -> String {
+fn calc_slope( run: &usize, rise: &usize ) -> f64{
+    let slope: f64;
+     slope = (*run as f64 / *rise as f64) as f64;
+     return slope;
+}
+pub fn attempt(dataset: Vec<String>, rise: usize, run: usize) -> f64 {
     //squares
     //value and location
     //
@@ -20,38 +25,50 @@ pub fn attempt(dataset: Vec<String>) -> String {
     let mut trees: Vec<TreeMark> = vec![];
     let mut hill: Vec<String> = vec![];
     let total_rows = dataset.len();
-    let mut counter = 0;
-    for data in dataset {
+    let slope: f64 = calc_slope(&run, &rise);
+    // x = (y+2)
+    let mut data_iter = dataset.iter().step_by(rise);
+    loop {
+        let data = match data_iter.next() {
+            Some(x) => x,
+            None => break,
+        };
         let pattern_length = data.len();
-        let target_col = calc_col_for_row(&counter);
-        let repeats_needed = target_col/pattern_length + 1;
+       // println!("slope: {}", slope);
+        let target_col = calc_col_for_row(&counter, &slope);
+        let repeats_needed = (target_col/pattern_length as f64) as usize + 1;
         let haystack = data.repeat(repeats_needed);
         let mats = re.find_iter(&haystack);
-        println!("{:?}", haystack);
+      //  println!("looking at row: {}, col: {}", counter, target_col);
+        //println!("haystack: {}", haystack);
         for mat in mats {
-            if mat.start() == target_col {
+            if mat.start() == target_col as usize {
+       //         println!("found match: {}, {:?}", counter, mat);
                 hits += 1;
+                let tree = TreeMark { x: mat.start(), y: counter };
+                trees.push(tree);
             }
-            //println!("found match: {:?}", mat);
-            let tree = TreeMark { x: mat.end(), y: counter };
-            trees.push(tree);
         }
-        counter += 1;
+        hill.push(haystack);
+        counter += rise;
     }
-    println!("trees: {:?}", trees);
+    //println!("trees: {:?}", trees);
+    //println!("number of trees: {:?}", trees.len());
+    for h in hill {
+        println!("{}", h);
+    }
     println!("After {} rows. There were {} hits", counter, hits);
-    return "OK".to_string();
+    return hits as f64;
 }
-fn calc_col_for_row(y: &usize) -> usize {
-   let x: usize = 3 * y;
-   return x;
+fn calc_col_for_row(y: &usize, m: &f64) -> f64 {
+   *m * *y as f64
 }
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
     fn attempt1() {
-        attempt(vec!["........#.....#...#......#....".to_string()]);
+        attempt(vec!["........#.....#...#......#....".to_string()], 0, 1);
         assert!(false);
     }
 }
